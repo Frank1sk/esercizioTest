cf79b2c521bdb7b861310140213411e518dd510f
Francesco Gionta <f.gionta90@gmail.com>
2024-06-14 09:57:21 +0200

    Creazione Notifica.java

---
+++ Notifica.java
@@ -0,0 +1,11 @@
+abstract class Notifica {
+    protected String destinatario;
+    protected String messaggio;
+
+    public Notifica(String destinatario, String messaggio) {
+        this.destinatario = destinatario;
+        this.messaggio = messaggio;
+    }
+
+    public abstract void invia();
+}
